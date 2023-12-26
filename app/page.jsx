541d922258347e7bdee3b2d3007876f72f348062
Mazen Z <96892759+Mazen-Z@users.noreply.github.com>
2023-12-26 16:04:59 -0500

    Initial commit for new repository

---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+
+const Home = () => {
+  return (
+    <section className='w-full flex-center flex-col'>
+    <h1 className='head_text text-center'>
+      BidBoats
+      <br className='max-md:hidden' />
+      <span className='gold_gradient text-center'> Buying. Made. Simple.</span>
+    </h1>
+    <p className='desc text-center'>
+    BidBoats is a online auction platform for all kinds of aquatic vehicles. 
+    Through a streamline and modern process, anyone can buy and sell boats for a small fee.
+    </p>
+  </section>
+  )
+}
+
+export default Home 
